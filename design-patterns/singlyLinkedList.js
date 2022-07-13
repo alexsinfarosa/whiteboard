b5a5fc76c9d355dab96fa386b12b3b3fa9f60e5e
Alex Sinfarosa <alexsinfarosa@users.noreply.github.com>
2022-07-13 21:44:52 +0200

    updates

--- design-patterns/singlyLinkedList.js
+++ design-patterns/singlyLinkedList.js
@@ -1,37 +1,91 @@
 class Node {
-  constructor(value) {
-    this.value = value
+  constructor(data) {
+    this.data = data
     this.next = null
   }
 }
-const node = new Node('A')
-console.log(node)
 
-class SinglyLinkedList {
+class LinkedList {
   constructor() {
     this.head = null
     this.tail = null
-    this.length = 0
+    this.size = 0
   }
 
-  push(val) {
-    const node = new Node(val)
+  unshift(data) {
+    const newNode = new Node(data)
     if (this.head === null) {
-      this.head = node
+      this.head = newNode
       this.tail = this.head
-    } else {
-      this.tail.next = node
-      this.tail = node
     }
-    this.length++
+    newNode.next = this.head
+    this.head = newNode
+    this.size++
     return this
+  }
+
+  push(data) {
+    const newNode = new Node(data)
+    if (this.head === null) {
+      this.head = newNode
+      this.tail = newNode
+    }
+    this.tail.next = newNode
+    this.tail = newNode
+    this.size++
+    return this
+  }
+
+  get(index) {
+    let current = this.head
+    let counter = 0
+    if (index < 0 || index === this.size) return null
+
+    while (counter !== index) {
+      current = current.next
+      counter++
+    }
+    return current
+  }
+
+  set(index, val) {
+    const found = this.get(index)
+    if (found) {
+      found.data = val
+      return true
+    }
+    return false
+  }
+
+  printData() {
+    let current = this.head
+    console.log(JSON.stringify(current))
+    while (current) {
+      console.log(`data:${current.data}`)
+      current = current.next
+    }
   }
 }
 
-const s = new SinglyLinkedList()
-s.push('B')
-console.log(s)
-s.push('C')
-console.log(s)
-s.push('D')
-console.log(s)
+const ll = new LinkedList()
+console.log(ll)
+
+ll.unshift(3)
+console.log(ll)
+
+ll.unshift(5)
+console.log(ll)
+
+ll.unshift(7)
+console.log(ll)
+
+ll.push(11)
+console.log(ll)
+
+ll.printData()
+
+const node3 = ll.get(3)
+console.log(node3)
+
+ll.set(0, 77)
+ll.printData()
